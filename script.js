340535e18ccd1fd3651e32ab839514e42753bab4
jjorgemc <jorgemaquerac@upeu.edu.pe>
2019-04-04 19:17:44 -0500

    Commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var productos = new Array();
 const igv = 0.18;
-var total;
-var totalIgv;
 const filas = document.getElementById('filas');
+//METODOS PRINCIPALES/////////////////////////////////////////////////////////////////////////////
 function respuestaBoton(){
-    agregarProducto();
-    vaciarTabla();
-    generarFilas();
+    if(areBoxesFilled){
+        agregarProducto();
+        vaciarTabla();
+        generarFilas();
+        limpiarCajas();
+    }
     //mostrarProductos();
 }
+//METODOS AGREGAR/////////////////////////////////////////////////////////////////////////////////
 function agregarProducto(){
     var nombre = document.getElementById('producto').value;
     var precio = document.getElementById('precio').value;
@@ -18,21 +21,80 @@ function agregarProducto(){
     productos[productos.length] = new Array(nombre, precio, cantidad, subtotal);
     alert(productos[productos.length-1].join(','));
 }
+//METODOS ELIMINAR////////////////////////////////////////////////////////////////////////////////
+function respuestaEliminar(index){
+    if(confirm('Desea eliminiar el producto: '+productos[index][0])){
+        productos.splice(index,1);
+        vaciarTabla();
+        generarFilas();
+        limpiarCajas();
+    }
+    
+
+}
+
+//METODOS MODIFICAR///////////////////////////////////////////////////////////////////////////////
+function respuestaModificar(index){
+    document.getElementById('producto').value = productos[index][0];
+    document.getElementById('precio').value = productos[index][1];
+    document.getElementById('cantidad').value = productos[index][2];
+    let boton = document.getElementById('sp_boton');
+    
+    boton.innerHTML = '<input id="boton" type="button" value="Modificar" onclick="respuestaBotonModificar('+index+')">';
+    
+}
+function modificarProducto(index){
+    productos[index][0] = document.getElementById('producto').value;
+    productos[index][1] = document.getElementById('precio').value;
+    productos[index][2] = document.getElementById('cantidad').value;
+    productos[index][3] = parseFloat(productos[index][1]) * parseInt(productos[index][2]);
+
+    let boton = document.getElementById('sp_boton');
+    boton.innerHTML = '<input type="button" id="boton" value="Agregar" onclick="respuestaBoton()"></input>';
+    
+}
+function respuestaBotonModificar(index){
+    if(areBoxesFilled){
+        modificarProducto(index);
+        vaciarTabla();
+        generarFilas();
+        limpiarCajas();
+    }
+}
+//METODOS AUXILIARES//////////////////////////////////////////////////////////////////////////////
+function generarFilas(){
+    let subtotala = 0;
+    for(var i=0; i < productos.length; i++){
+        var fila = document.createElement('tr');
+        fila.innerHTML = "<td>"+(i+1)+"</td><td>"+productos[i][0]+"</td><td>"+productos[i][1]+"</td><td>"+productos[i][2]+"</td><td>"+productos[i][3]+'</td><td><a href="#" onclick="respuestaModificar('+i+')"><i class="fas fa-pencil-alt"></i></a><a href="#" onclick="respuestaEliminar('+i+')"><i class="fas fa-times"></i></a></td>';
+        filas.appendChild(fila);
+        subtotala+=parseFloat(productos[i][3]);
+    }
+    document.getElementById('subtotal').innerHTML = subtotala
+    document.getElementById('igvtotal').innerHTML = subtotala*igv;
+    document.getElementById('total').innerHTML = subtotala + subtotala*igv;
+
+}
 function vaciarTabla(){
     document.getElementById('filas').innerHTML = "";
 }
-function mostrarProductos(){
-    for(var  i = 0; i < productos.length; i++){
-        var texto = document.createElement('span');
-        texto.innerHTML = productos[i].join(',');
-        document.getElementById('mostrar').appendChild('texto');
+function areBoxesFilled(){
+    let estado = false;
+    if(document.getElementById('producto').value != "" && document.getElementById('precio').value != "" && document.getElementById('cantidad').value != ""){
+        estado = true;
+    }else{
+        var form =  document.getElementById('formulario').getElementsByTagName('input');
+        for(var i = 0; i < form.length-1; i++){
+            if(form[i].value == ""){
+                alert(i+'vacio');
+                
+            }
+        }
     }
+    return estado;
 }
-function generarFilas(){
-    for(var i=0; i < productos.length; i++){
-        var fila = document.createElement('tr');
-        fila.innerHTML = "<td>"+(i+1)+"</td><td>"+productos[i][0]+"</td><td>"+productos[i][1]+"</td><td>"+productos[i][2]+"</td><td>"+productos[i][3]+"</td><td>"+""+"</td>";
-        filas.appendChild(fila);
-    }
-    alert('fin');
+function limpiarCajas(){
+    document.getElementById('producto').value = "";
+    document.getElementById('precio').value = "";
+    document.getElementById('cantidad').value = "";
 }
